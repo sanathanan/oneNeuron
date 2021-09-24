e05f860db658105fa4255295df718f461b91d884
Sanathanan <sanathanan.eee@gmail.com>
2021-09-24 16:58:24 +0530

    and file created

--- and.py
+++ and.py
@@ -1,5 +1,5 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
 import numpy as np
 
@@ -11,12 +11,17 @@ AND = {
 
 df = pd.DataFrame(AND)
 
-df
+print(df)
 
 X, y = prepare_data(df)
 
 ETA = 0.3 # 0 and 1
 EPOCHS = 10
 
-model = Perceptron(eta=eta, epochs=epochs)
+model = Perceptron(eta=ETA, epochs=EPOCHS)
 model.fit(X, y)
+
+_ = model.total_loss()
+
+save_model(model, filename="and.model")
+save_plot(df, "and.png", model)
--- utils/model.py
+++ utils/model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import logging
-from tqdm import tqdm
+
 
 class Perceptron:
   def __init__(self, eta, epochs):
@@ -21,7 +21,7 @@ class Perceptron:
     X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] # CONCATINATION
     print(f"X with bias: \n{X_with_bias}")
 
-    for epoch in tqdm(range(self.epochs), total=self.epochs, desc="training the model"):
+    for epoch in range(self.epochs):
       print("--"*10)
       print(f"for epoch: {epoch}")
       print("--"*10)
